89d86c125ca4da96d7f85ada5644cbc4a3bc6e9b
ikojun00 <ikojun00@fesb.hr>
2023-02-18 13:58:15 +0100

    Display and all buttons work know

--- calculator/script.js
+++ calculator/script.js
@@ -22,16 +22,21 @@ function operate(operation)
             console.log("Error: operate()");
             break;
     }
-    console.log(result);
     return result;
 }
 
 function inputOperand(operation, value)
 {
     if(operation.operator === "")
+    {
         operation.firstOperand = operation.firstOperand.concat(value);
+        document.getElementById("display").innerHTML = operation.firstOperand;
+    }
     else if(operation.operator != "")
+    {
         operation.secondOperand = operation.secondOperand.concat(value);
+        document.getElementById("display").innerHTML = operation.secondOperand;
+    }
     else
         console.log("Error: inputOperand()");
     console.log(operation);
@@ -39,30 +44,85 @@ function inputOperand(operation, value)
 
 function inputOperator(operation, value)
 {
-    if(operation.operator === "" && operation.firstOperand !== "")
+    if(operation.result !== "" && operation.firstOperand === "")
+    {
+        operation.firstOperand = operation.result;
+        operation.result = "";
+        operation.operator = value;
+    }
+    else if(operation.operator === "" && operation.firstOperand !== "")
         operation.operator = value;
     else if(operation.firstOperand !== "" && operation.secondOperand !== "")
     {
         operation.firstOperand = operate(operation);
         operation.secondOperand = "";    
         operation.operator = value;
+        document.getElementById("display").innerHTML = operation.firstOperand;
     }
     else
         console.log("Error: inputOperator()");
     console.log(operation);
 }
 
+function eraseInput(operation)
+{
+    operation.firstOperand="";
+    operation.secondOperand="";
+    operation.operator="";
+    operation.result ="";
+    document.getElementById("display").innerHTML = "";
+    console.log(operation);
+}
+
+function addSign(operation)
+{
+    if(operation.operator === "" && operation.firstOperand !== "")
+    {
+        if(operation.firstOperand.startsWith("-"))
+            operation.firstOperand = operation.firstOperand.slice(1);
+        else
+            operation.firstOperand = "-" + operation.firstOperand;
+        document.getElementById("display").innerHTML = operation.firstOperand;
+    }
+    else if(operation.operator !== "" && operation.secondOperand !== "")
+    {
+        if(operation.secondOperand.startsWith("-"))
+            operation.secondOperand = operation.secondOperand.slice(1);
+        else
+            operation.secondOperand = "-" + operation.secondOperand;
+        document.getElementById("display").innerHTML = operation.secondOperand;
+    }
+    else
+        console.log("Error: addSign()");
+    console.log(operation);
+}
+
+function solveWithPercent(operation)
+{
+    if(operation.firstOperand !== "" && operation.secondOperand === "")
+    {
+        operation.result = operation.firstOperand / 100;
+        document.getElementById("display").innerHTML = operation.result;
+        operation.firstOperand = "";
+    }
+    else
+        console.log("Error: solveWithPercent()");
+}
+
 function inputEquals(operation)
 {
     if(operation.operator !== "" && operation.firstOperand !== "" && operation.secondOperand !== "")
     {
-        operate(operation);
+        operation.result = operate(operation);
+        document.getElementById("display").innerHTML = operation.result;
         operation.firstOperand="";
         operation.secondOperand="";
         operation.operator="";
     }
-    console.log(operation);
+    else
+        console.log("Error: inputEquals()");  
 }
+
 
 function buttons(operation)
 {
@@ -81,6 +141,12 @@ function buttons(operation)
                 inputOperator(operation, document.getElementById(button.id).innerHTML);
             else if(button.id === "equals")
                 inputEquals(operation);
+            else if(button.id === "clear")
+                eraseInput(operation);
+            else if(button.id === "sign")
+                addSign(operation);
+            else if(button.id === "percent")
+                solveWithPercent(operation);
             else
                 return -1;
         });
@@ -90,6 +156,7 @@ function buttons(operation)
 let operation = {
     firstOperand: "",
     secondOperand: "",
-    operator: ""
+    operator: "",
+    result: ""
 };
 buttons(operation);
