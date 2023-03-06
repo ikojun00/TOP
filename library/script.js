0c1357f4c458d7ea8bb575a97297eeed05913332
ikojun00 <ikojun00@fesb.hr>
2023-03-06 11:36:12 +0100

    Implementation of remove and read buttons

--- library/script.js
+++ library/script.js
@@ -3,12 +3,17 @@ function myLibrary(latestBook) {
   const book = document.createElement('div');
   book.classList.add('card');
   const removeButton = document.createElement('div');
-  removeButton.classList.add('removeButton');
+  removeButton.classList.add('remove-button');
   const btnRemoveButton = document.createElement('button');
-  const textRemoveButton = document.createTextNode(`remove`);
+  const textRemoveButton = document.createTextNode('X');
   btnRemoveButton.appendChild(textRemoveButton);
   removeButton.appendChild(btnRemoveButton);
   book.appendChild(removeButton);
+  btnRemoveButton.addEventListener('click', (e) => {
+    e.target.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode
+    );
+  });
   const title = document.createElement('p');
   const textTitle = document.createTextNode(`Title: ${latestBook.title}`);
   title.appendChild(textTitle);
@@ -21,12 +26,27 @@ function myLibrary(latestBook) {
   const textPages = document.createTextNode(`Pages: ${latestBook.pages}`);
   pages.appendChild(textPages);
   book.appendChild(pages);
-  const isRead = document.createElement('p');
-  const textIsRead = document.createTextNode(
-    `Did you read it?: ${latestBook.isRead}`
-  );
-  isRead.appendChild(textIsRead);
+  const isRead = document.createElement('div');
+  isRead.classList.add('is-read-button');
+  const btnIsRead = document.createElement('button');
+  const textIsRead = document.createTextNode(`${latestBook.isRead}`);
+  if (`${latestBook.isRead}` === 'Not Read') {
+    btnIsRead.style.backgroundColor = '#FFCCCB';
+  } else if (`${latestBook.isRead}` === 'Read') {
+    btnIsRead.style.backgroundColor = 'lightgreen';
+  } else console.log('Error');
+  btnIsRead.appendChild(textIsRead);
+  isRead.appendChild(btnIsRead);
   book.appendChild(isRead);
+  btnIsRead.addEventListener('click', (e) => {
+    if (e.target.innerHTML === 'Not Read') {
+      e.target.innerHTML = 'Read';
+      e.target.style.background = 'lightgreen';
+    } else if (e.target.innerHTML === 'Read') {
+      e.target.innerHTML = 'Not Read';
+      e.target.style.background = '#FFCCCB ';
+    } else console.log(e.target.innerHTML);
+  });
   content.append(book);
 }
 
@@ -37,21 +57,19 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-  const form = document.getElementById('myForm');
-  const formData = new FormData(form);
+function addBookToLibrary(formData) {
   const book = {
     title: '',
     author: '',
     pages: '',
-    isRead: 'No',
+    isRead: 'Not Read',
   };
 
   for (const [key, value] of formData) {
     if (key === 'title') book.title = value;
     else if (key === 'author') book.author = value;
     else if (key === 'pages') book.pages = value;
-    else if (key === 'checkbox' && value === 'on') book.isRead = 'Yes';
+    else if (key === 'checkbox' && value === 'on') book.isRead = 'Read';
     else console.log('Error');
   }
 
@@ -77,8 +95,10 @@ function addEventListenerToButtons() {
   buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
       if (button.id === 'add-button') {
-        addBookToLibrary();
+        const form = document.getElementById('myForm');
+        const formData = new FormData(form);
         e.preventDefault();
+        addBookToLibrary(formData);
         closeForm();
       } else if (button.id === 'open-button') openForm();
       else if (button.id === 'close-button') closeForm();
