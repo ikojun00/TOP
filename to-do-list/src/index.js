e12c4a1df4e898fa6d64e886a33baa497db244b5
ikojun00 <ikojun00@fesb.hr>
2023-03-28 15:56:28 +0200

    add cards via form

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,13 +1,51 @@
-import { createCard } from './todos';
+import { createCard, openForm, removeAllChildNodes, closeForm } from './todos';
 
 function addEventListenerOnButtons() {
   const buttons = document.querySelectorAll('button');
 
   buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      if (button.id === 'add-button') createCard();
+    button.addEventListener('click', (e) => {
+      if (button.id === 'add-button') openForm();
+      else if (button.id === 'submit-button') {
+        const content = document.querySelector('#content');
+        removeAllChildNodes(content);
+        const form = document.getElementById('myForm');
+        const formData = new FormData(form);
+        e.preventDefault();
+        new Card().addCardToContent(formData);
+        closeForm();
+      }
     });
   });
 }
 
+class Card {
+  constructor(title, desc, priority) {
+    this.title = title;
+    this.desc = desc;
+    this.priority = priority;
+  }
+
+  addCardToContent(formData) {
+    const card = {
+      title: '',
+      desc: '',
+      priority: '',
+    };
+
+    for (const [key, value] of formData) {
+      if (key === 'title') card.title = value;
+      else if (key === 'desc') card.desc = value;
+      else if (key === 'priority') card.priority = value;
+      else console.log('Error');
+    }
+
+    const theHobbit = new Card(card.title, card.desc, card.priority);
+    cards.push(theHobbit);
+    for (let i = 0; i < cards.length; i += 1) createCard(cards[i]);
+    console.table(cards);
+  }
+}
+
+const cards = [];
 addEventListenerOnButtons();
--- to-do-list/src/todos.js
+++ to-do-list/src/todos.js
@@ -1,11 +1,33 @@
-function createCard() {
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function openForm() {
+  document.getElementById('myForm').style.display = 'flex';
+  document.getElementById('add-button').style.display = 'none';
+  document
+    .querySelectorAll('body >*:not(#myForm)')
+    .forEach((e) => (e.style.filter = 'blur(10px)'));
+}
+
+function closeForm() {
+  document.getElementById('myForm').style.display = 'none';
+  document.getElementById('add-button').style.display = 'flex';
+  document
+    .querySelectorAll('body >*')
+    .forEach((e) => (e.style.filter = 'blur(0)'));
+}
+
+function createCard(card) {
   const content = document.getElementById('content');
   const child = document.createElement('div');
   child.classList.add('card');
   child.innerHTML += `
             <div class="card-text">
                 <input type="checkbox" name="my-checkbox" id="opt-in"/>
-                <p>Title</p>
+                <p>${card.title}</p>
             </div>
             <div class="card-options">
                 <button>Details</button>
@@ -15,4 +37,4 @@ function createCard() {
   content.appendChild(child);
 }
 
-export { createCard };
+export { createCard, openForm, removeAllChildNodes, closeForm };
