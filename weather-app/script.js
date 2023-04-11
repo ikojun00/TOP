db62bc6eea56d61a049138ad9a4582b4e527a829
ikojun00 <ikojun00@fesb.hr>
2023-04-11 17:35:53 +0200

    basic info about weather and temp

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,25 +1,21 @@
 const Card = (weatherData) => {
-  const img = document.getElementById('condition-icon');
-  const conditionText = document.getElementById('condition-text');
-  const city = document.getElementById('city');
-  const region = document.getElementById('region');
-  const country = document.getElementById('country');
+  const img = document.getElementById('conditionIcon');
+  img.src = weatherData.current.condition.icon;
   const card = {
     city: weatherData.location.name,
     region: weatherData.location.region,
     country: weatherData.location.country,
-    data: weatherData,
-    conditionIcon: weatherData.current.condition.icon,
-    conditionText: weatherData.current.condition.text,
+    temp: `${weatherData.current.temp_c} °C`,
   };
-
-  city.textContent = card.city;
-  region.textContent = card.region;
-  country.textContent = card.country;
-  img.src = card.conditionIcon;
-  conditionText.textContent = card.conditionText;
+  const keys = Object.keys(card);
+  keys.forEach((key) => {
+    const content = document.getElementById(`${key}`);
+    content.innerHTML = '';
+    const text = document.createTextNode(`${card[key]}`);
+    content.appendChild(text);
+  });
   document.getElementById('content').style.display = 'flex';
-  console.log(card.data);
+  console.log(weatherData);
 };
 
 async function getWeather(city) {
