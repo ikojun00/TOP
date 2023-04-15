5646f8ac8e50959895eccc6bfd32163a2b28dd50
ikojun00 <ikojun00@fesb.hr>
2023-04-15 20:38:15 +0200

    recursion

---
+++ recursion/script.js
@@ -0,0 +1,20 @@
+function fibs(n)
+{
+    let array = [];
+    for(let i=0; i<n; i++)
+    {
+        if(i<2) array.push(i);
+        else array.push(array[i-1]+array[i-2]);
+    }
+    return array;
+}
+
+console.log(fibs(8));
+
+function fibsRec(n, array = [0, 1])
+{
+    if((array.length >= n)) return array;
+    return fibsRec(n, [...array, array[array.length - 2] + array[array.length - 1]]);
+}
+
+console.log(fibsRec(8));
