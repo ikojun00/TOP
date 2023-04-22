e201b0b59b8c6d58adb9318e552db6dca2679387
ikojun00 <ikojun00@fesb.hr>
2023-04-22 19:53:51 +0200

    head, tail, atIndex, pop, find implemented

--- linked-lists/script.js
+++ linked-lists/script.js
@@ -6,16 +6,20 @@ function Node(element = null, next = null)
 const LinkedList = () => {
     let head = null;
     let length = 0;
+    const iterateToEnd = () => {
+        let currentNode = head;
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    };
     const append = (element) => {
         const newNode = Node(element);
         if (head === null) {
             head = newNode;
         } 
         else {
-            let currentNode = head;
-            while (currentNode.next !== null) {
-                currentNode = currentNode.next;
-            }
+            const currentNode = iterateToEnd();
             currentNode.next = newNode;
         }
         length++;
@@ -26,6 +30,45 @@ const LinkedList = () => {
         length++;
     };
     const size = () => length;
+    const firstElement = () => head.element;
+    const lastElement = () => {
+        const currentNode = iterateToEnd();
+        return currentNode.element;
+    }
+    const indexOf = (index) => {
+        let i = 0;
+        let currentNode = head;
+        if (index < 0) return;
+        while (i < index) {
+            i += 1;
+            if (currentNode.next) {
+                currentNode = currentNode.next;
+            } 
+            else {
+                return null;
+            }
+        }
+        return currentNode.element;
+    }
+    const pop = () => {
+        let currentNode = head;
+        while (currentNode.next.next !== null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = null;
+    }
+    const find = (element) => {
+        let nodeIndex = 0;
+        let currentNode = head;
+        while (currentNode) {
+            if (element === currentNode.element) {
+                return nodeIndex;
+            }
+            nodeIndex++;
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
     const toString = () => {
         let result = "";
         let current = head;
@@ -35,12 +78,20 @@ const LinkedList = () => {
         }
         return result;
     };
-    return { append, size, toString, prepend };
+    return { append, prepend, size, firstElement, lastElement, indexOf, pop, find, toString };
 }
 
 const list = LinkedList();
-list.append(2);
-list.append(3);
+list.append(8);
+list.append(12);
+list.prepend(9);
 list.prepend(4);
+list.append(16);
+console.log(list.toString());
+list.pop();
 console.log(list.toString());
 console.log(list.size());
+console.log(list.firstElement());
+console.log(list.lastElement());
+console.log(list.indexOf(1));
+console.log(list.find(8));
