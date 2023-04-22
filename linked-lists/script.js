c734c59e4d1020ecd797b987e95547b70257e00a
ikojun00 <ikojun00@fesb.hr>
2023-04-22 19:15:07 +0200

    prepand and size

--- linked-lists/script.js
+++ linked-lists/script.js
@@ -1,23 +1,31 @@
+function Node(element = null, next = null)
+{
+    return {element, next};
+}
+
 const LinkedList = () => {
     let head = null;
     let length = 0;
     const append = (element) => {
-        const node = {
-            element,
-            next: null,
-        };
+        const newNode = Node(element);
         if (head === null) {
-            head = node;
+            head = newNode;
         } 
         else {
             let currentNode = head;
             while (currentNode.next !== null) {
                 currentNode = currentNode.next;
             }
-            currentNode.next = node;
+            currentNode.next = newNode;
         }
         length++;
     };
+    const prepend = (element) => {
+        const newNode = Node(element, head);
+        head = newNode;
+        length++;
+    };
+    const size = () => length;
     const toString = () => {
         let result = "";
         let current = head;
@@ -27,11 +35,12 @@ const LinkedList = () => {
         }
         return result;
     };
-    return { append, toString };
+    return { append, size, toString, prepend };
 }
 
 const list = LinkedList();
 list.append(2);
 list.append(3);
-list.append(4);
+list.prepend(4);
 console.log(list.toString());
+console.log(list.size());
