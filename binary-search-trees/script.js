85d121a5adc4c932b2154983b1767b967dd2b013
ikojun00 <ikojun00@fesb.hr>
2023-04-23 16:56:57 +0200

    insert and remove

--- binary-search-trees/script.js
+++ binary-search-trees/script.js
@@ -4,24 +4,17 @@ function Node(element = null, left = null, right = null)
 }
 
 function BuildTree(array, start = 0, end = array.length - 1) {
-  // Base case
-  if (start > end) return null;
+    if (start > end) return null;
 
-  // Get root node
-  const middle = Math.floor((start + end) / 2);
-  const root = Node(array[middle]);
-
-  // Recursion. Get left and right nodes
-  root.left = BuildTree(array, start, middle - 1);
-  root.right = BuildTree(array, middle + 1, end);
-
-  return root;
+    const middle = Math.floor((start + end) / 2);
+    const root = Node(array[middle]);
+    root.left = BuildTree(array, start, middle - 1);
+    root.right = BuildTree(array, middle + 1, end);
+    return root;
 }
 
 const BinarySearchTree = (array) => {
-    // Sort array numerically
     array = [...new Set(array.sort((a, b) => (a < b ? -1 : 1)))];
-    console.log(array);
 
     const root = () =>  BuildTree(array);
     const prettyPrint = (node, prefix = '', isLeft = true) => {
@@ -36,10 +29,57 @@ const BinarySearchTree = (array) => {
           prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
         }
     }
-    return { root , prettyPrint };
+    const insert = (value, position = root()) => {
+        if (value === position.element) return;
+
+        if (value < position.element) {
+            if (position.left === null) {
+                position.left = Node(value);
+                return;
+            }
+            insert(value, position.left);
+        }
+
+        if (value > position.element) {
+            if (position.right === null) {
+                position.right = Node(value);
+                return;
+            }
+            insert(value, position.right);
+        }
+        console.log(position);
+        return position;
+    };
+    const remove = (value, position = root()) => {
+        if (position === null) return position;
+
+        if (value < position.element) position.left = remove(value, position.left);
+        else if (value > position.element) position.right = remove(value, position.right);
+        
+        else 
+        {
+            if (position.left === null) return position.right;
+            else if (position.right === null) return position.left;
+
+            let key = position.right.element;
+            let root = position.right;
+
+            while (root.left !== null) {
+                key = root.left.element;
+                root = root.left;
+            }
+
+            position.element = key;
+            position.right = remove(key, position.right);
+        }
+        return position;
+    };
+    return { root, prettyPrint, insert, remove };
 }
 
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = BinarySearchTree(array);
 const root = tree.root();
 tree.prettyPrint(root);
+tree.prettyPrint(tree.insert(2));
+tree.prettyPrint(tree.remove(4));
