0174d09cd7775133f459cdeeb597b044e296f1f1
ikojun00 <ikojun00@fesb.hr>
2023-04-30 12:42:57 +0200

    tdd exercise: getting started and capitalize

---
+++ tdd-exercise/src/capitalize.js
@@ -0,0 +1,11 @@
+function capitalize(string){
+    // proci sve i prvi char prominiti za to upper
+    for(let i = 0; i<string.length; i+=1)
+    {
+        if(string[i] >= 'a' && string[i] <= 'z') return string.slice(0, i) + string[i].toUpperCase() + string.slice(i + 1, string.length + 1);
+        else if(string[i] >= 'A' && string[i] <= 'Z') return string;
+    }
+    return 'no valid characters';
+}
+   
+module.exports = capitalize;
