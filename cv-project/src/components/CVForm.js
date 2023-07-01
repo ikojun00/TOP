67193da3c5b73a20ccd06d0b1b93dc70bbd5a4f6
ikojun00 <ikojun00@fesb.hr>
2023-07-01 17:34:20 +0200

    feature: workExperience

--- cv-project/src/components/CVForm.js
+++ cv-project/src/components/CVForm.js
@@ -1,39 +1,79 @@
 import React, { Component } from 'react';
+import "../styles/index.css";
+import uniqid from "uniqid";
 import Overview from './Overview';
 
 class CVForm extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      formData: {
+      basicInfo: {
         firstName: 'John',
         lastName: 'Doe',
         email: 'johndoe@example.com',
+        phone: '+123 45 565 2322',
+        residence: 'Beograd, Serbia'
       },
+      workExperience: [{
+        id: uniqid(),
+        company: 'Google',
+        position: 'Software Engineer',
+        startDate: '2020',
+        endDate: '2021'
+      }]
     };
   }
 
-  handleChange = (e) => {
+  handleChangeOnBasicInfo = (e) => {
     const { name, value } = e.target;
     this.setState((prevState) => {
-      const updatedFormData = { ...prevState.formData, [name]: value };
-      return { formData: updatedFormData };
+      const updatedBasicInfo = { ...prevState.basicInfo, [name]: value };
+      return { basicInfo: updatedBasicInfo };
     });
   };
 
+  handleChangeOnWorkExperience = (event, index) => {
+    const { name, value } = event.target;
+    this.setState((prevState) => {
+      const updatedWorkExperience = [...prevState.workExperience];
+      updatedWorkExperience[index][name] = value;
+      return { workExperience: updatedWorkExperience };
+    });
+  };
+
+  handleAddInputOnWorkExperience = () => {
+    const { workExperience } = this.state;
+    const newWorkExperience = {
+      id: uniqid(),
+      company: 'Google',
+      position: 'Software Engineer',
+      startDate: '2020',
+      endDate: '2021'
+    };
+    this.setState({ workExperience: [...workExperience, newWorkExperience] });
+  };
+
+  handleRemoveInputOnWorkExperience = (id) => {
+    const { workExperience } = this.state;
+    const filteredworkExperience = workExperience.filter(input => input.id !== id);
+    this.setState({ workExperience: filteredworkExperience });
+  };
+
   render() {
-    const { firstName, lastName, email } = this.state.formData;
+    const { firstName, lastName, email, phone, residence } = this.state.basicInfo;
+    const { workExperience } = this.state;
 
     return (
-      <div>
-        <form>
+      <div className="cv">
+        <div className="cvEditor">
+          <h2>Personal details</h2>
           <label>
             First Name:
             <input
               type="text"
               name="firstName"
               value={firstName}
-              onChange={this.handleChange}
+              onChange={this.handleChangeOnBasicInfo}
             />
           </label>
           <br />
@@ -43,7 +83,7 @@ class CVForm extends Component {
               type="text"
               name="lastName"
               value={lastName}
-              onChange={this.handleChange}
+              onChange={this.handleChangeOnBasicInfo}
             />
           </label>
           <br />
@@ -53,12 +93,80 @@ class CVForm extends Component {
               type="email"
               name="email"
               value={email}
-              onChange={this.handleChange}
+              onChange={this.handleChangeOnBasicInfo}
             />
           </label>
           <br />
-        </form>
-        <Overview data={this.state.formData}/>
+          <label>
+            Phone:
+            <input
+              type="tel"
+              name="phone"
+              value={phone}
+              onChange={this.handleChangeOnBasicInfo}
+            />
+          </label>
+          <br />
+          <label>
+            Residence:
+            <input
+              type="text"
+              name="residence"
+              value={residence}
+              onChange={this.handleChangeOnBasicInfo}
+            />
+          </label>
+          <br />
+          <h2>Work experience</h2>
+          {workExperience.map((input, index) => (
+          <div key={input.id} className="cvEditor">
+            <label>
+              Company:
+              <input
+              type="text"
+              name="company"
+              value={input.company}
+              onChange={(event) => this.handleChangeOnWorkExperience(event, index)}
+              />
+            </label>
+            <br />
+            <label>
+              Position:
+              <input
+              type="text"
+              name="position"
+              value={input.position}
+              onChange={(event) => this.handleChangeOnWorkExperience(event, index)}
+              />
+            </label>
+            <br />
+            <label>
+              Start date:
+              <input
+              type="text"
+              name="startDate"
+              value={input.startDate}
+              onChange={(event) => this.handleChangeOnWorkExperience(event, index)}
+              />
+            </label>
+            <br />
+            <label>
+              End date:
+              <input
+              type="text"
+              name="endDate"
+              value={input.endDate}
+              onChange={(event) => this.handleChangeOnWorkExperience(event, index)}
+              />
+            </label>
+            <br />
+            <button onClick={() => this.handleRemoveInputOnWorkExperience(input.id)}>Remove</button>
+            <br />
+          </div>
+          ))}
+          <button onClick={this.handleAddInputOnWorkExperience}>Add Input</button>
+        </div>
+        <Overview data={this.state}/>
       </div>
     );
   }
--- cv-project/src/components/Overview.js
+++ cv-project/src/components/Overview.js
@@ -1,14 +1,36 @@
 import React from "react";
 
 const Overview = (props) => {
-  const { firstName, lastName, email } = props.data;
-  console.log(props)
-
+  console.log(props);
   return (
-    <div>
-        <p>First Name: {firstName}</p>
-        <p>Last Name: {lastName}</p>
-        <p>Email: {email}</p>
+    <div className="cvPaper">
+      <div className="header">
+        <div className="headerName">
+            <h1>{props.data.basicInfo.firstName} {props.data.basicInfo.lastName}</h1>
+        </div>
+        <div className="headerOther">
+            <p>{props.data.basicInfo.email}</p>
+            <p>{props.data.basicInfo.phone}</p>
+            <p>{props.data.basicInfo.residence}</p>
+        </div>
+      </div>
+      <h2>Work experience</h2>
+      <hr />
+      {props.data.workExperience.map((task) => (
+        <div key={task.id}>
+          <strong>{task.company}</strong>
+          <div className="workExperience">
+            <p>{task.position}</p>
+            <p>{task.startDate}-{task.endDate}</p>
+          </div>
+        </div>
+        ))}
+      <h2>Education</h2>
+      <hr />
+      <h2>Technical skills</h2>
+      <hr />
+      <h2>Other skills and interests</h2>
+      <hr />
     </div>
   );
 };
